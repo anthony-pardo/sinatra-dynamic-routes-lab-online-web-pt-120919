0148b1932e2cfc79f55ad414b54042b0ac5ac892
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-19 16:14:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,9 +18,8 @@ class App < Sinatra::Base
     "#{[@phrase] * @number * ' '}"
   end
   
-  get "/say/:number/:phrase" do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
     "#{[@phrase] * @number * ' '}"
   end
 end
