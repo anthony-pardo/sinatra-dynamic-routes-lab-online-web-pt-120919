bcc14475c19fa20ce1e42d27753c193663ca5345
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-19 16:21:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,5 +32,8 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
+    
+    result = @number1.send(@operation, @number2)
+    "#{result}"
   end
 end
