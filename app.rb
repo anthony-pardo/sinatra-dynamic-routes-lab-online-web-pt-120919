b0abff6f30efb47a32fc0947347bcefec555cc38
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-19 16:12:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    "#{@user_name}"
+    "#{[@phrase] * @number * ' '}"
   end
 end
